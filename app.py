5f51794b70b951c9f99f5e77695fcd1f1409b8a7
Masood85 <153002976+Masood85@users.noreply.github.com>
2024-02-27 21:31:30 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+header = st.container
+datasets = st.container
+features = st.container
+model_training = st.container
+
+
+
+
---
+++ test.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import seaborn as sns
+
+st.header('kia apko mza ah rha ha')
+st.text('Learning zone')
+df = sns.load_dataset('iris')
